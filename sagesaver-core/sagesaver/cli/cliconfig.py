75f6231fd8e6b5aeb5053b561ec1051b4129c82e
Cozieee <vvictor.llin@gmail.com>
2021-08-01 10:57:54 +0000

    feature(cli): add seed entity type as auxillary

--- sagesaver-core/sagesaver/cli/cliconfig.py
+++ sagesaver-core/sagesaver/cli/cliconfig.py
@@ -14,17 +14,17 @@ def load_config():
         with open(custom_conf_path, 'r') as f:
             custom_conf = json.load(f)
 
-    base_conf = {**base_conf, **custom_conf}
+        base_conf = {**base_conf, **custom_conf}
 
-    return {**base_conf, **custom_conf}
+    return base_conf
 
 def save_config(config):
     with open(entity_conf_path, 'w') as f:
-        f.write(config)
+        json.dump(config, f, indent=4)
 
 class ConfigManager():
     def __init__(self):
-        self.config = load_config
+        self.config = load_config()
 
     def update(self, scope, fields):
         if scope not in self.config:
--- sagesaver-core/sagesaver/cli/entitytypes.py
+++ sagesaver-core/sagesaver/cli/entitytypes.py
@@ -1,18 +1,19 @@
 from enum import Enum
 from sagesaver.entities.factories import *
-from ..installation import *
+from .installation import *
 
 class EntityTypes(Enum):
 	NOTEBOOK = ('notebook', 'Notebook', NotebookFactory, NotebookInstallation)
 	MYSQL = ('mysql', 'MySQL', MysqlFactory, MysqlInstallation)
 	MONGO = ('mongo', 'Mongo', MongoFactory, MongoInstallation)
 	USER = ('user', 'User', UserFactory, None)
+	SEED = ('seed', None, None, None)
 
-	def __init__(self, label, name, factory, installation):
+	def __init__(self, label, title, factory, installation):
 		self.label = label
-		self.name = name
+		self.title = title
 		self.factory = factory
 		self.installation = installation
 		
 	def __str__(self):
-		return self.name
+		return self.title
